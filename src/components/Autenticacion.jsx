dc5b6fa98cd9f5826ef919660e9d0f50242158d9
kevibaro <kevinbaza@hotmail.com>
2025-08-14 23:08:27 -0500

    estilos del google

--- src/components/Autenticacion.jsx
+++ src/components/Autenticacion.jsx
@@ -73,7 +73,7 @@ function Autenticacion() {
     // Si hay sesión → Mostrar perfil y botón de cerrar sesión
     return (
         <>
-            <div className="flex items-center justify-between gap-3 p-4 rounded-xl shadow-md border border-gray-200 bg-white ">
+            <div className="flex items-center justify-between gap-3 rounded-xl shadow-md border border-gray-200 bg-white ">
                 <Link
                     to="/"
                     className="text-2xl font-bold text-rose-500 hover:text-rose-600 transition"
