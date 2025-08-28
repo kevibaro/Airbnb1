2d9ba3fb120f1a8821a666e287db7ac9723249ef
kevibaro <kevinbaza@hotmail.com>
2025-08-27 19:27:28 -0500

    se realiza arreglo en el responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,36 @@
-
-import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
-
   return (
     <>
-      <div className=' flex justify-center  px-4'>
-        <div className='flex justify-center md:flex-row items-center  border-b-gray-400 w-full h-auto md:h-20 gap-4 md:gap-0'>
-          <Link to='/'><div>
-            <img className='relative flex  w-50 md:w-50' src={logo} alt="" />
-          </div></Link>
-        </div>
-      </div>
+      <header className="w-full border-b border-gray-200 shadow-sm bg-white">
+        <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-3 md:py-4">
+          {/* Logo */}
+          <Link to="/" className="flex items-center">
+            <img
+              className="w-28 md:w-36 object-contain"
+              src={logo}
+              alt="Logo"
+            />
+          </Link>
 
+          {/* Menú de navegación de ejemplo (se puede ampliar) */}
+          <nav className="hidden md:flex items-center gap-6">
+            <Link to="/" className="text-gray-700 hover:text-rose-500">
+              Inicio
+            </Link>
+            <Link to="/explorar" className="text-gray-700 hover:text-rose-500">
+              Explorar
+            </Link>
+            <Link to="/perfil" className="text-gray-700 hover:text-rose-500">
+              Perfil
+            </Link>
+          </nav>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
